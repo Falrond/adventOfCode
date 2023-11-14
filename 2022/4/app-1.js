26d889b05933ce7a66bf44e2a8fc22506dc9f7b2
Michał Winiecki <falrond@gmail.com>
2023-11-14 23:31:31 +0100

    solution 4a

---
+++ 2022/4/app-1.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+fs.readFile("./data.txt", "utf-8", (err, data) => {
+  const formatData = data
+    .split("\n")
+    .map((i) => i.replace("\r", ""))
+    .map((i) => i.split(","))
+    .reduce((acc, curr) => {
+      const curr1 = curr[0].split("-").map(Number);
+      const curr2 = curr[1].split("-").map(Number);
+
+      if (
+        (curr1[0] >= curr2[0] && curr1[1] <= curr2[1]) ||
+        (curr1[0] <= curr2[0] && curr1[1] >= curr2[1])
+      ) {
+        acc++;
+        console.log(curr1, curr2);
+      }
+      //   if (curr1[0] <= curr2[0] && curr1[1] >= curr2[1]) {
+      //     acc++;
+      //     console.log(curr1, curr2);
+      //   }
+      //   if (+curr[0][0] >= +curr[1][0] && +curr[0][2] <= +curr[1][2]) {
+      //     acc++;
+      //     console.log(curr);
+      //   }
+      //   if (+curr[0][0] <= +curr[1][0] && +curr[0][2] >= +curr[1][2]) {
+      //     acc++;
+      //     console.log(curr, " chuj");
+      //   }
+
+      return acc;
+    }, 0);
+  console.log(formatData);
+});
